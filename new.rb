c7309c27ece65575992eeed8d35b783758bf0461
code <33802433+codeacade@users.noreply.github.com>
2019-03-13 02:03:20 +0000

    Caesar cipher

--- new.rb
+++ new.rb
@@ -1,2 +1,22 @@
 aa = "aa"
 p aa
+alph = ("a".."z").to_a            # create array with alphabet
+caesar = puts.chomp                 # input string to cypher
+caesar.split('').each do |i|         # itterate on string to cypher
+  puts alph[alph.index(i).to_i + 2]  # replace character fron string with 2 position higher (a -> c)
+end
+
+
+# not sure whats that?
+
+%w { 1 2 3 4 }.each do |num|
+s3_file "/usr/local/tomcat7-7/part[num].tif"
+remote_path "part[num].tif"
+bucket "perftest-data"
+aws_access_key_id "XXXXXXX"
+aws_secret_access_key "ZZZZZZZZ"
+owner "root"
+group "root"
+mode "0644"
+action :create
+end
