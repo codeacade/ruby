bb17d4e4860a75785c0e14b41a75bf8424d9a719
code <33802433+codeacade@users.noreply.github.com>
2019-03-25 02:01:46 +0000

    Xmas tree added
    Multiply "string" by "string"*10  :)

--- new.rb
+++ new.rb
@@ -6,8 +6,11 @@ caesar.split('').each do |i|         # itterate on string to cypher
   puts alph[alph.index(i).to_i + 2]  # replace character fron string with 2 position higher (a -> c)
 end
 
-Time.now   # returns curent time
+# Xmass tree
 
+30.times {|i| puts " "*(30-i) + "X"*i*2}
+
+Time.now   # returns curent time
 Date.today   # return current date
 required 'date'  # must load this first to make date.today working
 
