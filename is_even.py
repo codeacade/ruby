5e636dd4310d0a65edcb77233663646e42c0147b
code <33802433+codeacade@users.noreply.github.com>
2020-02-15 01:48:36 +0000

    python-exercises
    from Codecademy

---
+++ is_even.py
@@ -0,0 +1,12 @@
+# https://www.codecademy.com/courses/learn-python/lessons/practice-makes-perfect/exercises/iseven
+#
+# Define a function is_even that will take a number x as input.
+# If x is even, then return True.
+# Otherwise, return False.
+
+def is_even(x):
+    if x %2 == 0:
+        return True
+    return False
+print is_even(900)
+
