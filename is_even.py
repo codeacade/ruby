76f8130d6f7a0b6367ce9c69f59ef6326fedcf38
code <33802433+codeacade@users.noreply.github.com>
2020-02-15 02:27:12 +0000

    first 3 exercises
    from codeacade

--- is_even.py
+++ is_even.py
@@ -10,3 +10,25 @@ def is_even(x):
     return False
 print is_even(900)
 
+# https://www.codecademy.com/courses/learn-python/lessons/practice-makes-perfect/exercises/isint
+#
+# Define a function is_int that takes a number x as an input.
+# Have it return True if the number is an integer (as defined above) and False otherwise.
+
+def is_int(x):
+  return (x%1==0)
+
+# https://www.codecademy.com/courses/learn-python/lessons/practice-makes-perfect/exercises/digitsum
+#
+# Write a function called digit_sum that takes a positive integer n as input and returns the sum of all that number’s digits. 
+# For example: digit_sum(1234) should return 10 which is 1 + 2 + 3 + 4. 
+# (Assume that the number you are given will always be positive.)
+
+def digit_sum(n):
+  result=0
+  for i in str(n):
+    result+=int(i)
+  return result
+
+
+
