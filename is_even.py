f439b80ace930e06427109e4a521f220f40f15c1
code <33802433+codeacade@users.noreply.github.com>
2020-02-18 00:57:59 +0000

    this is probably twelfth exercise
    from codecademy

--- is_even.py
+++ is_even.py
@@ -151,4 +151,17 @@ def purify(numbers):
   return result
 
 
+# https://www.codecademy.com/courses/learn-python/lessons/practice-makes-perfect/exercises/product
+# 
+# Define a function called product that takes a list of integers as input and returns the product of all of the elements in the list. 
+# For example: product([4, 5, 5]) should return 100 (because 4 * 5 * 5 is 100).
+# Your function should return an integer.
+
+def product(numbers):
+  result = 1
+  for i in numbers:
+    result*=i
+  return result
+
+###### SUPEREASY #######
 
