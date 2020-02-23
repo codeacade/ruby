a27ebd9f3f191e6b54c0129acdaa32d6ec48deed
code <33802433+codeacade@users.noreply.github.com>
2020-02-23 02:09:57 +0000

    comments fixed

--- is_even.py
+++ is_even.py
@@ -128,9 +128,9 @@ def scrabble_score(word):
 # 
 # Write a function called censor that takes two strings, text and word, as input. 
 # It should return the text with the word you chose replaced with asterisks. For example:
-censor("this hack is good hacker hack", "hack")
-should return: 
-"this **** is good hacker ****"
+#  censor("this hack is good hacker hack", "hack")
+# should return: 
+#  "this **** is good hacker ****"
 # Assume your input strings won’t contain punctuation or upper case letters.
 # The number of asterisks you put should correspond to the number of letters in the censored word.
 
