07435d0df64c318776416f94b85db5c275ba2b7e
code <33802433+codeacade@users.noreply.github.com>
2020-02-25 14:20:42 +0000

    Part 10 - lambdas and other "advanced" elements
    From codeacade

---
+++ lambdas.py
@@ -0,0 +1,5 @@
+# All new world of lambdas, the Anonymous Functions...
+# https://www.codecademy.com/courses/learn-python/lessons/advanced-topics-in-python/exercises/anonymous-functions
+
+my_list = range(16)
+print filter(lambda x: x % 3 == 0, my_list)
