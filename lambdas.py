fc12e00c7f820c105c000a5f8190ce2ac499368d
code <33802433+codeacade@users.noreply.github.com>
2020-02-27 21:28:05 +0000

    more lambdas added
    from codecade

--- lambdas.py
+++ lambdas.py
@@ -4,4 +4,17 @@
 my_list = range(16)
 print filter(lambda x: x % 3 == 0, my_list)
 
-# comments added
+# Fill in the first part of the filter function with a lambda. 
+# The lambda should ensure that only "Python" is returned by the filter.
+
+languages = ["HTML", "JavaScript", "Python", "Ruby"]   # -given
+print(filter(lambda x:x == "Python", languages))
+
+
+# Create a list, squares, that consists of the squares of the numbers 1 to 10. 
+# A list comprehension could be useful here!
+# Use filter() and a lambda expression to print out only the squares that are between 30 and 70 (inclusive).
+
+sqlist = [i*i for i in range(1,11)]
+print(filter(lambda x: x>30 and x<70,sqlist))
+
