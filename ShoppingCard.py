199eb79a67876fe7711207923e507443decfdd23
code <33802433+codeacade@users.noreply.github.com>
2020-03-07 01:51:18 +0000

    fors commit for ShoppingCard
    from codeacade

---
+++ ShoppingCard.py
@@ -0,0 +1,33 @@
+# this is ready-to-go example from codeacademy
+# link: 
+# https://www.codecademy.com/courses/learn-python/lessons/introduction-to-classes/exercises/its-not-all-animals-and-fruits
+#
+# but still there is something to do:
+#
+# Create an instance of ShoppingCart called my_cart. 
+# Initialize it with any values you like,
+# then use the add_item method to add an item to your cart.
+
+
+class ShoppingCart(object):
+  """Creates shopping cart objects
+  for users of our fine website."""
+  
+  def __init__(self, customer_name):
+    self.customer_name = customer_name
+    self.items_in_cart = {}
+  def add_item(self, product, price):
+    """Add product to the cart."""
+    if not product in self.items_in_cart:
+      self.items_in_cart[product] = price
+      print product + " added."
+    else:
+      print product + " is already in the cart."
+
+  def remove_item(self, product):
+    """Remove product from the cart."""
+    if product in self.items_in_cart:
+      del self.items_in_cart[product]
+      print product + " removed."
+    else:
+      print product + " is not in the cart."
