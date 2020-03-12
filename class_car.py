47a9548cc3887e0071e55b54035412a7ddcfe1b3
code <33802433+codeacade@users.noreply.github.com>
2020-03-12 01:38:49 +0000

    Car class and methods
    from codecademy

---
+++ class_car.py
@@ -0,0 +1,17 @@
+
+# Pre-created class "Car":
+
+class Car(object):
+  condition = "new"
+  def __init__(self, model, color, mpg):
+    self.model = model
+    self.color = color
+    self.mpg   = mpg
+    
+  # New method that shows all "Car"'s properties in kind verbose mode.
+  def display_car(self):
+    return "This is a {} {} with {} MPG.".format(self.color, self.model, self.mpg)
+
+# Creating instance of "Car" method and calling it's methods:
+my_car = Car("DeLorean", "silver", 88)
+print my_car.display_car()
