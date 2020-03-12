20ea0a8c710f728dcccc1b4c91eb12479d56cada
code <33802433+codeacade@users.noreply.github.com>
2020-03-12 01:56:30 +0000

    methods added to "car" class

--- class_car.py
+++ class_car.py
@@ -11,7 +11,13 @@ class Car(object):
   # New method that shows all "Car"'s properties in kind verbose mode.
   def display_car(self):
     return "This is a {} {} with {} MPG.".format(self.color, self.model, self.mpg)
-
+  # New method that sets "condition" variable i cars was used.
+  def drive_car(self):
+    self.condition = "used"
+    
+    
 # Creating instance of "Car" method and calling it's methods:
 my_car = Car("DeLorean", "silver", 88)
-print(my_car.display_car())
+print my_car.condition
+my_car.drive_car()
+print my_car.condition
